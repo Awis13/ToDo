250cfef72e5dbe0059e4aae5c8d49aa79ae17e8f
Nipostni <awis@me.com>
2023-09-25 13:52:11 +0200

    port check

--- todo_test.py
+++ todo_test.py
@@ -1,71 +1,51 @@
 import unittest
-import app  # Import your Flask application
 import json
+from app import app, get_db  # Import your Flask application and database function
 
 class TestApp(unittest.TestCase):
-    def setUp(self):
-        self.app = app.app.test_client()
+	def setUp(self):
+		self.app = app.test_client()
 
-    def test_create_task(self):
-        response = self.app.post('/tasks', data=json.dumps({
-            'title': 'Test Task',
-            'description': 'This is a test task',
-            'due_date': '2023-06-19',
-            'priority': 'High'
-        }), content_type='application/json')
-        self.assertEqual(response.status_code, 201)
-        self.app.delete('/tasks/1')  # Clean up after test
+	def tearDown(self):
+		with app.app_context():
+			db = get_db()
+			db.execute('DELETE FROM tasks')
+			db.commit()
 
-    def test_get_task(self):
-        self.app.post('/tasks', data=json.dumps({
-            'title': 'Test Task',
-            'description': 'This is a test task',
-            'due_date': '2023-06-19',
-            'priority': 'High'
-        }), content_type='application/json')
-        response = self.app.get('/tasks/1')
-        self.assertEqual(response.status_code, 200)
-        data = json.loads(response.data)
-        self.assertEqual(data['title'], 'Test Task')
-        self.app.delete('/tasks/1')  # Clean up after test
+	def test_create_task(self):
+		response = self.app.post('/tasks', data=json.dumps({
+			'title': 'Test Task',
+			'description': 'This is a test task',
+			'due_date': '2023-06-19',
+			'priority': 'High'
+		}), content_type='application/json')
+		self.assertEqual(response.status_code, 201)
 
-    def test_delete_task(self):
-        self.app.post('/tasks', data=json.dumps({
-            'title': 'Test Task',
-            'description': 'This is a test task',
-            'due_date': '2023-06-19',
-            'priority': 'High'
-        }), content_type='application/json')
-        response = self.app.delete('/tasks/1')
-        self.assertEqual(response.status_code, 204)
-    def test_create_invalid_task(self):
-        response = self.app.post('/tasks', data=json.dumps({
-            'description': 'This is a test task',
-            'due_date': '2023-06-19',
-            'priority': 'High'
-        }), content_type='application/json')
-        self.assertEqual(response.status_code, 400)  # Expect a 400 Bad Request status code
+	def test_get_task(self):
+		response = self.app.post('/tasks', data=json.dumps({
+			'title': 'Test Task',
+			'description': 'This is a test task',
+			'due_date': '2023-06-19',
+			'priority': 'High'
+		}), content_type='application/json')
+		task_id = json.loads(response.data)['id']
+		response = self.app.get(f'/tasks/{task_id}')
+		self.assertEqual(response.status_code, 200)
+		data = json.loads(response.data)
+		self.assertEqual(data['title'], 'Test Task')
 
-    def test_update_task(self):
-        self.app.post('/tasks', data=json.dumps({
-            'title': 'Test Task',
-            'description': 'This is a test task',
-            'due_date': '2023-06-19',
-            'priority': 'High'
-        }), content_type='application/json')
-        response = self.app.put('/tasks/1', data=json.dumps({
-            'title': 'Updated Test Task',
-            'description': 'This is an updated test task',
-            'due_date': '2023-06-20',
-            'priority': 'Low'
-        }), content_type='application/json')
-        self.assertEqual(response.status_code, 200)  # Expect a 200 OK status code
-        data = json.loads(response.data)
-        self.assertEqual(data['title'], 'Updated Test Task')  # Check that the title was updated
+	def test_delete_task(self):
+		response = self.app.post('/tasks', data=json.dumps({
+			'title': 'Test Task',
+			'description': 'This is a test task',
+			'due_date': '2023-06-19',
+			'priority': 'High'
+		}), content_type='application/json')
+		task_id = json.loads(response.data)['id']
+		response = self.app.delete(f'/tasks/{task_id}')
+		self.assertEqual(response.status_code, 204)
 
-    def test_get_non_existent_task(self):
-        response = self.app.get('/tasks/999')
-        self.assertEqual(response.status_code, 404)  # Expect a 404 Not Found status code
+	# ... (rest of your test methods)
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
